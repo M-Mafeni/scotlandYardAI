a66e7760aa5e6caf22614e618dce319cb9c1e380
michael <mm17291@bristol.ac.uk>
2018-04-04 18:08:48 +0100

    got Dijkstra's algorithm to work now

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Dijkstra.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Dijkstra.java
@@ -16,16 +16,53 @@ public class Dijkstra {
         visited = new HashSet<>(); //no nodes are yet to be visited
         unvisited = new HashSet<>(gameMap.getNodes()); //all nodes are unvisited at the start
         graph = gameMap;
-        initDistances(distances,unvisited);
+        initDistances();
+        while (unvisited.size()> 0) { // while there are still unvisited nodes
+            visitUnknown();
+        }
         //For now let's assume that all edges have a weight of 1
     }
-    private void initDistances(Map<Node<Character>,Integer> map,Set<Node<Character>> unvisited){
+    //visits unvisited node with shortest distance from the source node
+    private void visitUnknown(){
+        // unvisited node with shortest distance from source node
+        Node<Character> currentNode = getUnvisitedNode();
+        Collection<Edge<Character,Integer>> edges = graph.getEdgesFrom(currentNode);
+        Set<Node<Character>> neighbours;
+        for(Edge<Character,Integer> e: edges){
+            int distance = distances.get(currentNode); // you want the distance from the
+                                                      // current node to its unvisited neighbours
+            Node<Character> neighbour = e.destination();
+            if(unvisited.contains(neighbour)){ //if the neighbouring node is unvisited
+                distance += e.data(); //add the distance from
+                //if distance gotten is less than distance stored update it
+                if(distance < distances.get(neighbour)){
+                    distances.put(neighbour,distance);
+                }
+            }
+        }
+        visited.add(currentNode); //add current node to visited
+        unvisited.remove(currentNode); // remove current node from unvisited
+    }
+
+    //get unvisited node by finding shortest distance from source node
+    private Node<Character> getUnvisitedNode(){
+        int min = INF;
+        Node<Character> minNode = null;
+        for(Node<Character> u: unvisited){
+            if(distances.get(u) < min) {
+                minNode = u;
+            }
+        }
+        return minNode;
+    }
+    //initialises the distance map
+    private void initDistances(){
         //distances from source node should be infinity at start
         for(Node<Character> u: unvisited){
             if(u.equals(sourceNode)) //distance from source node should be zero at first
-                map.put(u,0);
+                distances.put(u,0);
             else
-                map.put(u,INF); //distance from all other nodes should be infinity
+                distances.put(u,INF); //distance from all other nodes should be infinity
         }
     }
 
--- src/test/DijkstraTest.java
+++ src/test/DijkstraTest.java
@@ -1,23 +1,12 @@
 import org.junit.Test;
 import uk.ac.bris.cs.gamekit.graph.*;
-import uk.ac.bris.cs.scotlandyard.model.*;
 import uk.ac.bris.cs.scotlandyard.ui.ai.Dijkstra;
-
-import static uk.ac.bris.cs.scotlandyard.model.Transport.TAXI;
 import java.util.*;
 
 
 public class DijkstraTest{
-    @Test
-    public void testInitialDistances(){
-        Graph<Character,Integer> testGraph = new UndirectedGraph<>();
-        List<Node<Character>> nodes = new ArrayList<>();
-        addNodes(testGraph,nodes);
-        addEdges(testGraph,nodes);
-        Dijkstra testDijkstra = new Dijkstra(testGraph,nodes.get(0));
-        checkDistances(testDijkstra.getDistances(),testDijkstra.getSourceNode());
 
-    }
+    //nodes based on pic I sent you
     private void addNodes(Graph<Character,Integer> testGraph,List<Node<Character>> nodes){
         for(int a = 1;a <= 5;a++){
             int asc = a + 64; //  to get ASCII code
@@ -26,6 +15,7 @@ public class DijkstraTest{
             nodes.add(new Node<>(letter));
         }
     }
+    //edges based on pic I sent you
     private void addEdges(Graph<Character,Integer> testGraph,List<Node<Character>> nodes){
         testGraph.addEdge(new Edge<>( nodes.get(0),nodes.get(3), 1));
         testGraph.addEdge(new Edge<>(nodes.get(0),nodes.get(1),6));
@@ -35,14 +25,22 @@ public class DijkstraTest{
         testGraph.addEdge(new Edge<>(nodes.get(3),nodes.get(4),1));
         testGraph.addEdge(new Edge<>(nodes.get(2),nodes.get(4),5));
     }
-    private void checkDistances(Map<Node<Character>, Integer> distances,Node<Character> sourceNode){
-        //checks distances initially
-        Set<Node<Character>> nodes = distances.keySet();
-        for(Node<Character> n:nodes){
-            if(n.equals(sourceNode))
-                assert(distances.get(n) == 0);
-            else
-                assert(distances.get(n) == 1000); // Value of INF
-        }
+
+    // to check graph works correctly at the end
+    @Test
+    public void checkFinalDistances(){
+        Graph<Character,Integer> testGraph = new UndirectedGraph<>();
+        List<Node<Character>> nodes = new ArrayList<>();
+        addNodes(testGraph,nodes);
+        addEdges(testGraph,nodes);
+        Dijkstra testDijkstra = new Dijkstra(testGraph,nodes.get(0));
+        Map<Node<Character>, Integer> distances = testDijkstra.getDistances();
+        assert(distances.get(new Node<>('A')) == 0);
+        assert(distances.get(new Node<>('B')) == 3);
+        assert(distances.get(new Node<>('C')) == 7);
+        assert(distances.get(new Node<>('D')) == 1);
+        assert(distances.get(new Node<>('E')) == 2);
     }
+
+
 }
